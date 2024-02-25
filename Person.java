8cb960e6b0b5259c307dbaef192784b057cdcea0
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-02-25 22:26:23 +0100

    Update Person.java

--- Person.java
+++ Person.java
@@ -3,30 +3,38 @@ package my.game;
 import java.util.Objects;
 
 public abstract class Person {
-  private String id;
-  private String name;
+    private String id;
+    private String name;
 
-  public Person(String id, String name) {
-      String newID = "";
-      for (int i = 0; i < id.length(); i++) {
-          char c = id.charAt(i);
-          if (Character.isDigit(c) || Character.isAlphabetic(c)) {
-              newID += c;
-          }
-      }
-    if (newID.length() > 3) {
-        this.id = newID.substring(0, 3).toUpperCase();
-    } else {
-        this.id = newID.toUpperCase();
+    public Person(String id, String name) {
+        String newID = "";
+        for (int i = 0; i < id.length(); i++) {
+            char c = id.charAt(i);
+            if (Character.isDigit(c) || Character.isAlphabetic(c)) {
+                newID += c;
+            }
+        }
+        if (newID.length() > 3) {
+            this.id = newID.substring(0, 3).toUpperCase();
+        } else {
+            this.id = newID.toUpperCase();
+        }
+        this.name = name;
     }
-      this.name = name;
-  }
 
-  public String getId() {
-      return id;
-  }
+    public String getId() {
+        return id;
+    }
 
     public String getName() {
-      return name;
-  }
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
 }
