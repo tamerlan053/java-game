0069313433c41e0c6b21e4858c685abbdee512f9
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-03-03 18:44:22 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,4 +2,9 @@ package my.game;
 
 public class Player extends Person {
     private Sport sport;
+
+    public Player(String id, String name, Sport sport) {
+        super(id, name);
+        this.sport = sport;
+    }
 }
