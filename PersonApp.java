68e139aa45a29bed4fef14462e5496c196e86f37
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-03-05 22:18:20 +0100

    Update PersonApp.java

--- PersonApp.java
+++ PersonApp.java
@@ -2,6 +2,6 @@ package my.game;
 
 public class PersonApp {
       public static void main(String[] args) {
-        Player player1 = new Speler ("A17AA", "Rafel Nadal");
+        Player player1 = new Speler ("A17AA", "Rafel Nadal", Sport.TENNIS);
       }
 }
