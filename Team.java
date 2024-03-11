23e9da3ca974ce430ec0fdf7b7b5f778d31522b0
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-03-11 17:18:07 +0100

    Update Team.java

--- Team.java
+++ Team.java
@@ -3,4 +3,9 @@ package my.game;
 public class Team {
     private Sport sport;
     private Player[] players ;
+
+    public Team(Sport sport) {
+        this.sport = sport;
+        this.players = new Player[sport.getNumberOfPlayers()];
+    }
 }
