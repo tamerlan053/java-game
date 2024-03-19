ea90bd56b2898c18c03f8361fbbf1c551d59cec7
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-03-19 20:54:53 +0100

    Update TeamApp.java

--- TeamApp.java
+++ TeamApp.java
@@ -6,5 +6,6 @@ public class TeamApp {
         Player player2 = new PLayer("L96PO", "Bill Tilden", Sport.TENNIS);
         Player player3 = new PLayer("CR7", "Cristiano Ronaldo", Sport.FOOTBALL);
         Player player4 = new PLayer("Y98RE", "Bjorn Bog", Sport.TENNIS);
+        Player player5 = new PLayer("P11WE", "Rod Laver", Sport.TENNIS);
     }
 }
