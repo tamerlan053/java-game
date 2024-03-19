c629f43148a9a1c3135ff3e5bd92beca54a07d49
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-03-19 20:52:50 +0100

    Update TeamApp.java

--- TeamApp.java
+++ TeamApp.java
@@ -3,5 +3,6 @@ package my.game;
 public class TeamApp {
     public static void main(String[] args) {
         Player player1 = new PLayer("A12LA", "Andre Agassi", Sport.TENNIS);
+        Player player2 = new PLayer("L96PO", "Bill Tilden", Sport.TENNIS);
     }
 }
