f26e3595f0be0ec5052c6d72c9ee65d29847fcf5
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-03-20 23:48:06 +0100

    Update TeamApp.java

--- TeamApp.java
+++ TeamApp.java
@@ -7,5 +7,16 @@ public class TeamApp {
         Player player3 = new PLayer("CR7", "Cristiano Ronaldo", Sport.FOOTBALL);
         Player player4 = new PLayer("Y98RE", "Bjorn Bog", Sport.TENNIS);
         Player player5 = new PLayer("P11WE", "Rod Laver", Sport.TENNIS);
+
+        team1.addPlayer(player1);
+        System.out.println(team1);
+        team2.addPlayer(player2);
+        System.out.println(team2);
+        team3.addPlayer(player3);
+        System.out.println(team3);
+        team4.addPlayer(player4);
+        System.out.println(team4);
+        team5.addPlayer(player5);
+        System.out.println(team5);
     }
 }
