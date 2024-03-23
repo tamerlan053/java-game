69b16108c5630239efa9af2942c57ed5b8fe6fc5
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-03-23 22:19:56 +0100

    Update Competition.java

--- Competition.java
+++ Competition.java
@@ -6,4 +6,5 @@ import java.time.format.DateTimeFormatter;
 
 public class Competition implements DateComparable {
   private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
+  private LocalDateTime competitionDate;
 }
