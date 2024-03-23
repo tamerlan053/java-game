84a9d56f61e267e089939d3dc61bc6b79d44633f
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-03-23 22:19:08 +0100

    Update Competition.java

--- Competition.java
+++ Competition.java
@@ -3,3 +3,7 @@ package my.game;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
+public class Competition implements DateComparable {
+  
+}
