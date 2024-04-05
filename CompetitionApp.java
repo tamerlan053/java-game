a7a45295f0bbde2803cfa9fc10eeb5df6e9b0458
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-04-05 14:56:02 +0400

    Update CompetitionApp.java

--- CompetitionApp.java
+++ CompetitionApp.java
@@ -5,5 +5,7 @@ public class CompetitionApp {
         Player player1 = new PLayer("A12LA", "Andre Agassi", Sport.TENNIS);
         Player player2 = new PLayer("L96PO", "Bill Tilden", Sport.TENNIS);
         Team team1 = new Team(Sport.TENNIS);
+        team1.addPLayer(player1);
+        team1.addPLayer(player2);
     }
 }
