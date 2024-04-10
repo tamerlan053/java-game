3ff59abe455d36391a688b498c1bde64bd7763b4
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-04-10 23:08:03 +0400

    Update Tournament.java

--- Tournament.java
+++ Tournament.java
@@ -3,4 +3,8 @@ package my.game;
 public class Tournament {
     private Competition[] competitions;
     private static final int MAX_COMPETITIONS = 10;
+
+    public Tournament() {
+        this.competitions = new Competition[MAX_COMPETITIONS];
+    }
 }
