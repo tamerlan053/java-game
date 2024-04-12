764f37405139196862e0f0402fc02a6a5237bcf3
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-04-12 15:41:34 +0400

    Update TournamentApp.java

--- TournamentApp.java
+++ TournamentApp.java
@@ -4,5 +4,6 @@ public class TournamentApp {
     public static void main(String[] args) {
         Player player1 = new PLayer("A12LA", "Andre Agassi", Sport.TENNIS);
         Player player2 = new PLayer("L96PO", "Bill Tilden", Sport.TENNIS);
+        team1.voegSpelerToe(speler1);
     }
 }
