020efb6ac2d7e32d5c08208eb2c47e3124860bd2
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-04-13 00:43:46 +0400

    Update TeamApp.java

--- TeamApp.java
+++ TeamApp.java
@@ -2,11 +2,11 @@ package my.game;
 
 public class TeamApp {
     public static void main(String[] args) {
-        Player player1 = new PLayer("A12LA", "Andre Agassi", Sport.TENNIS);
-        Player player2 = new PLayer("L96PO", "Bill Tilden", Sport.TENNIS);
-        Player player3 = new PLayer("CR7", "Cristiano Ronaldo", Sport.FOOTBALL);
-        Player player4 = new PLayer("Y98RE", "Bjorn Bog", Sport.TENNIS);
-        Player player5 = new PLayer("P11WE", "Rod Laver", Sport.TENNIS);
+        Player player1 = new Player("A12LA", "Andre Agassi", Sport.TENNIS);
+        Player player2 = new Player("L96PO", "Bill Tilden", Sport.TENNIS);
+        Player player3 = new Player("CR7", "Cristiano Ronaldo", Sport.FOOTBALL);
+        Player player4 = new Player("Y98RE", "Bjorn Bog", Sport.TENNIS);
+        Player player5 = new Player("P11WE", "Rod Laver", Sport.TENNIS);
 
         team1.addPlayer(player1);
         System.out.println(team1);
