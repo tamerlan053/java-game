a917cdbcea0606904a21868031655f70d75f3137
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-04-12 15:43:28 +0400

    Update TournamentApp.java

--- TournamentApp.java
+++ TournamentApp.java
@@ -6,5 +6,8 @@ public class TournamentApp {
         Player player2 = new PLayer("L96PO", "Bill Tilden", Sport.TENNIS);
         team1.addPlayer(player1);
         team1.addPlayer(player2);
+
+        Player player3 = new PLayer("Y98RE", "Bjorn Bog", Sport.TENNIS);
+        Player player4 = new PLayer("P11WE", "Rod Laver", Sport.TENNIS);
     }
 }
