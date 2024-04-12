bf3cae37dff4403f575b2c0ca46fcd23f9263df8
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-04-13 00:48:16 +0400

    Update CompetitionApp.java

--- CompetitionApp.java
+++ CompetitionApp.java
@@ -2,14 +2,14 @@ package my.game;
 
 public class CompetitionApp {
     public static void main(String[] args) {
-        Player player1 = new PLayer("A12LA", "Andre Agassi", Sport.TENNIS);
-        Player player2 = new PLayer("L96PO", "Bill Tilden", Sport.TENNIS);
+        Player player1 = new Player("A12LA", "Andre Agassi", Sport.TENNIS);
+        Player player2 = new Player("L96PO", "Bill Tilden", Sport.TENNIS);
         Team team1 = new Team(Sport.TENNIS);
         team1.addPLayer(player1);
         team1.addPLayer(player2);
 
-        Player player3 = new PLayer("Y98RE", "Bjorn Bog", Sport.TENNIS);
-        Player player4 = new PLayer("P11WE", "Rod Laver", Sport.TENNIS);
+        Player player3 = new Player("Y98RE", "Bjorn Bog", Sport.TENNIS);
+        Player player4 = new Player("P11WE", "Rod Laver", Sport.TENNIS);
         Team team2 = new Team(Sport.TENNIS);
         team2.addPLayer(player3);
         team2.addPLayer(player4);
