cb60172b9db7fd0400157695d2e18f8c8a875837
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-04-12 15:41:09 +0400

    Update TournamentApp.java

--- TournamentApp.java
+++ TournamentApp.java
@@ -2,6 +2,7 @@ package my.game;
 
 public class TournamentApp {
     public static void main(String[] args) {
-      
+        Player player1 = new PLayer("A12LA", "Andre Agassi", Sport.TENNIS);
+        Player player2 = new PLayer("L96PO", "Bill Tilden", Sport.TENNIS);
     }
 }
